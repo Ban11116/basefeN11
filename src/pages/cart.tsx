f990ec106feddfed921d7967fc84667b7d90fc76
Your Name <you@example.com>
2025-05-26 17:54:38 +0700

    Sửa cart.tsx

--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -22,9 +22,9 @@ const Cart = () => {
         Giỏ hàng của bạn
       </h1>
 
-      <div className="overflow-x-auto bg-[#2b2b2b] rounded-md shadow-md">
-        <div className="min-w-[600px]">
-          <table className="w-full border text-sm text-left text-white">
+      <div className="overflow-x-auto bg-[#2b2b2b] rounded-md shadow-md max-h-[400px] overflow-y-auto">
+  <div className="min-w-[600px]">
+    <table className="w-full border text-sm text-left text-white">
             <thead className="bg-[#333] text-orange-300">
               <tr>
                 <th className="p-3">SẢN PHẨM</th>
@@ -34,6 +34,138 @@ const Cart = () => {
               </tr>
             </thead>
             <tbody>
+              <tr className="border-t border-[#444]">
+                <td className="p-3 flex items-center gap-2">
+                  <img
+                    src="image/products1.png"
+                    alt="Nước hoa CK One 100ml"
+                    className="w-16 h-16 object-cover rounded"
+                  />
+                  Nước hoa CK One 100ml
+                </td>
+                <td className="p-3 text-orange-400">850.000 ₫</td>
+                <td className="p-3">
+                  <input
+                    type="number"
+                    value={1}
+                    className="w-16 sm:w-20 border border-gray-500 bg-[#1c1c1c] text-white px-2 py-1 rounded"
+                  />
+                </td>
+                <td className="p-3 text-orange-400">850.000 ₫</td>
+              </tr>
+              <tr className="border-t border-[#444]">
+                <td className="p-3 flex items-center gap-2">
+                  <img
+                    src="image/products1.png"
+                    alt="Nước hoa CK One 100ml"
+                    className="w-16 h-16 object-cover rounded"
+                  />
+                  Nước hoa CK One 100ml
+                </td>
+                <td className="p-3 text-orange-400">850.000 ₫</td>
+                <td className="p-3">
+                  <input
+                    type="number"
+                    value={1}
+                    className="w-16 sm:w-20 border border-gray-500 bg-[#1c1c1c] text-white px-2 py-1 rounded"
+                  />
+                </td>
+                <td className="p-3 text-orange-400">850.000 ₫</td>
+              </tr>
+              <tr className="border-t border-[#444]">
+                <td className="p-3 flex items-center gap-2">
+                  <img
+                    src="image/products1.png"
+                    alt="Nước hoa CK One 100ml"
+                    className="w-16 h-16 object-cover rounded"
+                  />
+                  Nước hoa CK One 100ml
+                </td>
+                <td className="p-3 text-orange-400">850.000 ₫</td>
+                <td className="p-3">
+                  <input
+                    type="number"
+                    value={1}
+                    className="w-16 sm:w-20 border border-gray-500 bg-[#1c1c1c] text-white px-2 py-1 rounded"
+                  />
+                </td>
+                <td className="p-3 text-orange-400">850.000 ₫</td>
+              </tr>
+              <tr className="border-t border-[#444]">
+                <td className="p-3 flex items-center gap-2">
+                  <img
+                    src="image/products1.png"
+                    alt="Nước hoa CK One 100ml"
+                    className="w-16 h-16 object-cover rounded"
+                  />
+                  Nước hoa CK One 100ml
+                </td>
+                <td className="p-3 text-orange-400">850.000 ₫</td>
+                <td className="p-3">
+                  <input
+                    type="number"
+                    value={1}
+                    className="w-16 sm:w-20 border border-gray-500 bg-[#1c1c1c] text-white px-2 py-1 rounded"
+                  />
+                </td>
+                <td className="p-3 text-orange-400">850.000 ₫</td>
+              </tr><tr className="border-t border-[#444]">
+                <td className="p-3 flex items-center gap-2">
+                  <img
+                    src="image/products1.png"
+                    alt="Nước hoa CK One 100ml"
+                    className="w-16 h-16 object-cover rounded"
+                  />
+                  Nước hoa CK One 100ml
+                </td>
+                <td className="p-3 text-orange-400">850.000 ₫</td>
+                <td className="p-3">
+                  <input
+                    type="number"
+                    value={1}
+                    className="w-16 sm:w-20 border border-gray-500 bg-[#1c1c1c] text-white px-2 py-1 rounded"
+                  />
+                </td>
+                <td className="p-3 text-orange-400">850.000 ₫</td>
+              </tr>
+              <tr className="border-t border-[#444]">
+                <td className="p-3 flex items-center gap-2">
+                  <img
+                    src="image/products1.png"
+                    alt="Nước hoa CK One 100ml"
+                    className="w-16 h-16 object-cover rounded"
+                  />
+                  Nước hoa CK One 100ml
+                </td>
+                <td className="p-3 text-orange-400">850.000 ₫</td>
+                <td className="p-3">
+                  <input
+                    type="number"
+                    value={1}
+                    className="w-16 sm:w-20 border border-gray-500 bg-[#1c1c1c] text-white px-2 py-1 rounded"
+                  />
+                </td>
+                <td className="p-3 text-orange-400">850.000 ₫</td>
+              </tr>
+              <tr className="border-t border-[#444]">
+                <td className="p-3 flex items-center gap-2">
+                  <img
+                    src="image/products1.png"
+                    alt="Nước hoa CK One 100ml"
+                    className="w-16 h-16 object-cover rounded"
+                  />
+                  Nước hoa CK One 100ml
+                </td>
+                <td className="p-3 text-orange-400">850.000 ₫</td>
+                <td className="p-3">
+                  <input
+                    type="number"
+                    value={1}
+                    className="w-16 sm:w-20 border border-gray-500 bg-[#1c1c1c] text-white px-2 py-1 rounded"
+                  />
+                </td>
+                <td className="p-3 text-orange-400">850.000 ₫</td>
+              </tr>
               <tr className="border-t border-[#444]">
                 <td className="p-3 flex items-center gap-2">
                   <img
